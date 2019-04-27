6792267554460e82ba31854290842da763fc45f1
TrySpace <mgasman@gmail.com>
2019-04-27 14:18:43 +0200

    Why u inherit

--- src/game.ts
+++ src/game.ts
@@ -2,8 +2,17 @@ import { GameUtils } from './game-utils';
 import * as BABYLON from 'babylonjs';
 import * as GUI from 'babylonjs-gui';
 import { Shark } from './shark';
+import { RenderCanvas } from './RenderCanvas';
+import { Scene } from './scene';
+import { Camera } from './camera';
+import { World } from './world';
 
 export class Game {
+
+    private renderCanvas: RenderCanvas;
+    private renderScene: Scene;
+    private camera: Camera;
+    private world: World;
 
     private _canvas: HTMLCanvasElement;
     private _engine: BABYLON.Engine;
@@ -19,8 +28,10 @@ export class Game {
 
     constructor(canvasElement: string) {
         // Create canvas and engine
-        this._canvas = <HTMLCanvasElement>document.getElementById(canvasElement);
-        this._engine = new BABYLON.Engine(this._canvas, true);
+        this.renderCanvas = new RenderCanvas(canvasElement);
+        this._canvas = this.renderCanvas.canvas;
+        this._engine = this.renderCanvas.engine;
+
     }
 
     /**
@@ -28,50 +39,57 @@ export class Game {
      */
     createScene(): void {
         // create a basic BJS Scene object
-        this._scene = new BABYLON.Scene(this._engine);
+        this.renderScene = new Scene(this.renderCanvas);
+        this._scene = this.renderScene.scene//new BABYLON.Scene(this._engine);
+
         // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-        this._camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 30, BABYLON.Vector3.Zero(), this._scene);
-        this._camera.attachControl(this._canvas, true);
+        // this._camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 30, BABYLON.Vector3.Zero(), this._scene);
+        // this._camera.attachControl(this._canvas, true);
+        this.camera = new Camera(this.renderScene, 1);
+
+
+        this.world = new World(this.renderScene, true, true);
+
         // create a basic light, aiming 0,1,0 - meaning, to the sky
-        this._light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
-        // create the skybox
-        let skybox = GameUtils.createSkybox("skybox", "./assets/texture/skybox/TropicalSunnyDay", this._scene);
-
-        // creates the sandy ground
-        let ground = GameUtils.createGround(this._scene);
-        // creates the watermaterial and adds the relevant nodes to the renderlist
-        let waterMaterial = GameUtils.createWater(this._scene);
-        waterMaterial.addToRenderList(skybox);
-        waterMaterial.addToRenderList(ground);
-
-        this._shark = new Shark(this._engine, this._scene);
-        this._shark.createShark(this._scene, waterMaterial);
-        // finally the new ui
-        let guiTexture = GameUtils.createGUI();
-
-        // Button to start shark animation
-        GameUtils.createButtonSwim(guiTexture, "Start Swimming",
-            (btn) => {
-                let textControl = btn.children[0] as GUI.TextBlock;
-                this._shark._swim = !this._shark._swim;
-                if (this._shark._swim) {
-                    textControl.text = "Stop Swimming";
-                }
-                else {
-                    this._shark._sharkAnimationTime = 0;
-                    textControl.text = "Start Swimming";
-                }
-            });
-
-        // Debug Text for Shark coordinates
-        this._txtCoordinates = GameUtils.createCoordinatesText(guiTexture);
-
-        // Physics engine also works
-        let gravity = new BABYLON.Vector3(0, -0.9, 0);
-        this._scene.enablePhysics(gravity, new BABYLON.CannonJSPlugin());
-
-
-        this.animate();
+        // this._light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this._scene);
+        // // create the skybox
+        // let skybox = GameUtils.createSkybox("skybox", "./assets/texture/skybox/TropicalSunnyDay", this._scene);
+        //
+        // // creates the sandy ground
+        // let ground = GameUtils.createGround(this._scene);
+        // // creates the watermaterial and adds the relevant nodes to the renderlist
+        // let waterMaterial = GameUtils.createWater(this._scene);
+        // waterMaterial.addToRenderList(skybox);
+        // waterMaterial.addToRenderList(ground);
+        //
+        // this._shark = new Shark(this._engine, this._scene);
+        // this._shark.createShark(this._scene, waterMaterial);
+        // // finally the new ui
+        // let guiTexture = GameUtils.createGUI();
+        //
+        // // Button to start shark animation
+        // GameUtils.createButtonSwim(guiTexture, "Start Swimming",
+        //     (btn) => {
+        //         let textControl = btn.children[0] as GUI.TextBlock;
+        //         this._shark._swim = !this._shark._swim;
+        //         if (this._shark._swim) {
+        //             textControl.text = "Stop Swimming";
+        //         }
+        //         else {
+        //             this._shark._sharkAnimationTime = 0;
+        //             textControl.text = "Start Swimming";
+        //         }
+        //     });
+        //
+        // // Debug Text for Shark coordinates
+        // this._txtCoordinates = GameUtils.createCoordinatesText(guiTexture);
+        //
+        // // Physics engine also works
+        // let gravity = new BABYLON.Vector3(0, -0.9, 0);
+        // this._scene.enablePhysics(gravity, new BABYLON.CannonJSPlugin());
+        //
+        //
+        // this.animate();
 
     }
 
--- src/index.ts
+++ src/index.ts
@@ -13,26 +13,29 @@ window.addEventListener('DOMContentLoaded', () => {
   window.CANNON = CANNON;
   let game = new Game('renderCanvas');
   game.createScene();
+
+  let game2 = new Game('renderCanvas2');
+  game2.createScene();
   // game.animate();
 
   // let game2 = new Game('renderCanvas2');
   // game2.createScene();
   // game2.animate();
 
-  let renderCanvas2 = new RenderCanvas('renderCanvas2');
+  // let renderCanvas2 = new RenderCanvas('renderCanvas2');
   let renderCanvas3 = new RenderCanvas('renderCanvas3');
   let renderCanvas4 = new RenderCanvas('renderCanvas4');
 
-  let scene2 = new Scene(renderCanvas2);
+  // let scene2 = new Scene(renderCanvas2);
   let scene3 = new Scene(renderCanvas3);
   let scene4 = new Scene(renderCanvas4);
 
-  let camera2 = new Camera(scene2, 2);
+  // let camera2 = new Camera(scene2, 2);
   let camera3 = new Camera(scene3, 3);
   let camera4 = new Camera(scene4, 4);
 
   // Fill world
-  let world2 = new World(scene2, true, true);
+  // let world2 = new World(scene2, true, true);
   let world3 = new World(scene3, false, false);
   let world4 = new World(scene4, true, false);
 
